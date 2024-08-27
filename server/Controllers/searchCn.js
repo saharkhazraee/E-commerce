b53c2fee1cf58f6a88ed9b6a7484dd4dc6fd7583
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-08-27 15:39:27 +0330

    Update searchCn.js

--- server/Controllers/searchCn.js
+++ server/Controllers/searchCn.js
@@ -3,7 +3,7 @@ import Product from "../Models/productMd.js";
 import catchAsync from "../Utils/catchAsync.js";
 
 export const search=catchAsync(async(req,res,next)=>{
-    const search=req.body
+    const {search}=req.body
     const productResult=await Product.find({name:{$regex:search}})
     const categoryResult=await Category.find({name:{$regex:search}})
      return res.status(200).json({productResult,categoryResult})
