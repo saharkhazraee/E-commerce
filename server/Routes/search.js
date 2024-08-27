dc60b9b9a3a5030a0cb69872a7beefe0e052ef95
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-08-27 15:40:39 +0330

    Update search.js

--- server/Routes/search.js
+++ server/Routes/search.js
@@ -1,5 +1,6 @@
 import express from 'express'
+import { search } from '../Controllers/searchCn.js'
 const searchRouter=express.Router()
-
+searchRouter.route('/').get(search)
 
 export default searchRouter
