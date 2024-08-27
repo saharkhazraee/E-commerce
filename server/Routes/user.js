c123ffdf81a4f502e77442644ec741ffaa434c75
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-08-27 13:17:49 +0330

    Update user.js

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import { getAllUSer, getOneUser, updateUser } from '../Controllers/userCn.js'
+import isAdmin from '../Middleware/isAdmin.js'
+import isLogin from '../Middleware/isLogin.js'
 const userRouter=express.Router()
-
+userRouter.route('/').get(isAdmin,getAllUSer)
+userRouter.route('/:id').get(isLogin,getOneUser).patch(isLogin,updateUser)
 
 export default userRouter
